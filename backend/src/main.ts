7b364d92fc97e90f2e7455f3c588cca02847294e
Douglas Reis <douglas.reiscrq@gmail.com>
2025-11-20 19:58:46 -0300

    feat: introducing api structure

---
+++ backend/src/main.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import { photographerRoutes } from "./infrastructure/http/routes/photographer-routes";
+
+const app = express();
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use("/api/photographers", photographerRoutes);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
